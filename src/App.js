2a5b273ee94379c153e88a854d2da394f76fcb72
seeunjang <wisjang1@naver.com>
2022-10-05 12:18:42 +0900

    load kakaomap & marker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+/*global kakao*/ 
+import React, { useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Location=()=>{
+
+  useEffect(()=>{
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3
+    };
+
+    var map = new kakao.maps.Map(container, options);
+    var markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
+    var marker = new kakao.maps.Marker({
+      position: markerPosition
+  });
+  marker.setMap(map);
+
+    }, [])
+
+
+    return (
+        <div>
+        <div id="map" style={{width:"500px", height:"400px"}}></div>
+       
+        </div>
+    )
 }
 
-export default App;
+export default Location;
